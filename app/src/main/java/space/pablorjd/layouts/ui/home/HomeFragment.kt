badbbb3aca0dcc07d9db0da32f9f2b3ca439a4dd
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-08-24 12:31:48 -0400

    *LinearLayout* es un grupo de vistas que alinea todos los elementos secundarios en una única dirección, de manera vertical u horizontal. Puedes especificar la dirección del diseño con el atributo android:orientation. Todos los elementos secundarios de un LinearLayout se apilan uno detrás de otro, por lo cual una lista vertical solo tendrá un elemento secundario por fila, independientemente del ancho que tengan, y una lista horizontal solo tendrá la altura de una fila (la altura del elemento secundario más alto, más el padding).

--- app/src/main/java/space/pablorjd/layouts/ui/home/HomeFragment.kt
+++ app/src/main/java/space/pablorjd/layouts/ui/home/HomeFragment.kt
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import space.pablorjd.layouts.databinding.FragmentHomeBinding
+
+import space.pablorjd.layouts.databinding.FragmentLinearBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentLinearBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +26,7 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentLinearBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
