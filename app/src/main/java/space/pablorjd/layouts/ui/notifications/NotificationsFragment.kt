af86bfce3ba3075aebf71494c43ed02f20afe287
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-08-28 21:11:14 -0400

    constraint_layout

--- app/src/main/java/space/pablorjd/layouts/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/space/pablorjd/layouts/ui/notifications/NotificationsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import space.pablorjd.layouts.databinding.FragmentNotificationsBinding
+import space.pablorjd.layouts.databinding.FragmentConstrainBinding
 
 class NotificationsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentConstrainBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class NotificationsFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentConstrainBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
